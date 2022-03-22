f46581d3283b8ee60a6f03319ded2ba4d4856c6b
naamorim <naamorim2013@hotmail.com>
2022-03-21 21:00:45 -0300

    UC8 A1

---
+++ par.js
@@ -0,0 +1,12 @@
+const quantidadedealunos = 14 
+var alunos = 14;
+for (let numero =0; numero <= 14; numero ++)
+if (numero ==0){
+    console.log ("número zero");
+}
+else if (numero %2 ==0){
+    console.log("O número é par: " + numero);
+}
+else {
+    console.log ("O número é impar" + numero);
+}
